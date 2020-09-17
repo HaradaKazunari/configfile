d779f8da3f5e69168b67c709eafa640f8b6d5ad0
harada.k <dbxkazuxdb@gamil.com>
2020-09-17 16:03:03 +0900

    edit config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -26,3 +26,17 @@ function setup
   curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
   fisher add jethrokuan/z && fisher add decors/fish-ghq && fisher add 0rax/fish-bd && fisher add fishpkg/fish-spin && fisher add oh-my-fish/plugin-balias && fisher add oh-my-fish/plugin-expand && fisher add oh-my-fish/plugin-peco && fisher add simnalamburt/shellder
 end
+
+function ide
+  tmux split-window -h
+  tmux resize-pane -L 55 
+  tmux split-window -v
+  tmux resize-pane -D 20 
+  tmux split-window -h
+  tmux select-pane -t 1
+  tmux split-window -h
+  tmux select-pane -t 3
+  tmux resize-pane -R 20 
+  tmux select-pane -t 4
+end
+
